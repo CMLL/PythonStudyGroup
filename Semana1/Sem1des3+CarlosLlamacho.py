939974d87bf48c5e1a110f6895c3975037794b06
cllamach <llamacholancho@gmail.com>
2013-04-25 03:51:58 +0000

    Añadido el desafio 3 del grupo Python. Diseño general del programa hecho. Falta algoritmo central.

---
+++ Semana1/Sem1des3+CarlosLlamacho.py
@@ -0,0 +1,35 @@
+#Grupo de estudio de Python
+#Semana 1 - Desafio 3
+
+#Ingresar cuatro valores y que dibuje 4 barras del tamaño de los valores ingresados.
+
+#Realizado por Carlos Llamacho
+
+# -*- coding: utf8 -*-
+
+barras = []
+
+def desafio3():
+
+    maximo = int(input("Tamaño maximo: "))
+
+    for item in range(4):
+        barras.append(int(input("Introduzca el tamaño de la barra " + str(item + 1) + ": ")))
+
+    print("*" * 22)
+    for i in range(maximo):
+    	print("ffuuuu")
+    print("*" * 22)
+
+
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    desafio3()
+
