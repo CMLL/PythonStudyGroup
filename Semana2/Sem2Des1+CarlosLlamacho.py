b8d67bd262fc3c1fd03a0314ca3bc0827792e486
CMLL <llamacholancho@gmail.com>
2013-05-02 20:52:14 +0000

    Añdadidos los comentarios del codigo.

--- Semana2/Sem2Des1+CarlosLlamacho.py
+++ Semana2/Sem2Des1+CarlosLlamacho.py
@@ -10,8 +10,11 @@ def binario(decimal):
 	
 	resultado = ""
 	
+	#Loop para el algoritmo.
 	while decimal != 0:
+		#Resto del numero a convertir, este es el numero binario.
 		resultado += str(decimal % 2)
+		#Esto reduce el numero decimal hasta que se convierte en 0.
 		decimal = decimal // 2
 		
 	return resultado
