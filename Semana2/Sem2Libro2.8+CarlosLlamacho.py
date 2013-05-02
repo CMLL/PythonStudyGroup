f312d3c7b572c2dab77a5f2c1138f84bff4af23b
CMLL <llamacholancho@gmail.com>
2013-05-02 19:21:20 +0000

    Comentado el codigo.

--- Semana2/Sem2Libro2.8+CarlosLlamacho.py
+++ Semana2/Sem2Libro2.8+CarlosLlamacho.py
@@ -9,10 +9,15 @@ de esos amigos/as, y los salude."""
 #Carlos Llamacho
 
 def saludar():
+	#Recoge la cantidad de personas a saludar.
 	amigos = int(input("A cuantos amigos quiere saludar: "))
 	
+	#Loop que los saluda a todos.
 	for i in range(1, amigos+1):
+		
+		#Pregunta el nombre.
 		nombre = str(input("Cual es tu nombre: "))
+		#Saluda.
 		print("Hola " + nombre + ". Bienvenido/a.")
 		print("*" * 25)
 
