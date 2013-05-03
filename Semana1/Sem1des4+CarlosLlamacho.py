b52f917f7967e2f7eba37240ba1ab4b0a87817ef
cllamach <llamacholancho@gmail.com>
2013-05-03 03:33:55 +0000

    Arreglado desafio 1 de la segunda semana. Agregada funcion que convierte de binario a decimal. Agregada funcion que muestra un numero binario al reves. Comentado el codigo.

--- Semana1/Sem1des4+CarlosLlamacho.py
+++ Semana1/Sem1des4+CarlosLlamacho.py
@@ -7,8 +7,14 @@
 
 def desafio4():
 	
+	numero = int(input("Dame un numero: "))
 	
+	lista = [range(1, numero+1)]
 	
+	for i in range(numero):
+		print(" " * (numero- i) + str(i+1) + " " * (numero - i))
+
+
 
 if __name__=="__main__":
 	desafio4()
--- Semana2/Sem2Des1+CarlosLlamacho.py
+++ Semana2/Sem2Des1+CarlosLlamacho.py
@@ -2,7 +2,7 @@
 
 #Unidad 2 - Desafio 1
 
-"""Escribir un programa que convierta de numeros decimales a binarios."""
+"""Escribir un programa que convierta de numeros decimales a binarios y viceversa."""
 
 #Carlos Llamacho
 
@@ -16,10 +16,49 @@ def binario(decimal):
 		resultado += str(decimal % 2)
 		#Esto reduce el numero decimal hasta que se convierte en 0.
 		decimal = decimal // 2
-		
+
+        resultado = virar(resultado)
 	return resultado
 
+#Funcion que convierte de binario a decimal.
+def decimal(binario):
+    
+    #Para fines de conversion virar el binario.
+    binario = virar(binario)
+
+    #Convertirlo en string para fines de iteracion
+    binario = str(binario)
+
+    #Variables para usar en proceso.
+    decimal = 0
+    tam = range(len(binario))
+    counter = len(binario)
+
+    #Loop principal.
+    for num in binario:
+        decimal += int(num) * (2**tam[-counter])
+        counter -= 1
+
+    #Numero decimal.
+    return decimal
+
+#Funcion que coge un numero binario y lo vira al reves.
+def virar(binario):
+
+    #Convertirlo a string para fines de iteracion.
+    binario = str(binario)
+    tam = len(binario) - 1
+    binario_reves = ""
+
+    #Loop principal.
+    for num in binario:
+        binario_reves += binario[tam]     
+        tam -= 1
+
+    return binario_reves
+
+
 if __name__=="__main__":
-	decimal = int(input("Introduce un numero decimal: "))
+	binario = int(input("Introduce un numero binario: "))
 	
-	print(binario(decimal))
+	print(decimal(binario))
