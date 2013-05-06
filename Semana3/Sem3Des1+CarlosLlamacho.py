c5bcfc0377df9596daf43cb8c1870b2ada9919ba
CMLL <llamacholancho@gmail.com>
2013-05-06 20:27:59 +0000

    Añadido el primer desafio de la tercera semana. Verdadero si un # es primo, falso de lo contrario. No puede usar ifs.

---
+++ Semana3/Sem3Des1+CarlosLlamacho.py
@@ -0,0 +1,22 @@
+#Grupo de estudio de Python G+.
+
+#Unidad 3 - Desafio 1
+
+"""es_primo
+Un número primo es un entero positivo mayor que 1 que no tiene divisores positivos distintos al 1 y a sí mismo.
+En otras palabras, si deseas saber si un número en una variable x es primo, entonces ningún otro número debería poder dividir x con residuo cero aparte del 1 y de x.
+Si hay un número entre 1 y x que pueda dividirlo con residuo cero, entonces x no es primo.
+Instrucciones
+Escribe una función es_primo que tome un número x como entrada y devuelva el booleano Verdadero si x es primo y Falso si no lo es."""
+
+#Carlos Llamacho
+
+def esprimo(numero):
+
+
+
+if __name__=="__main__":
+	
+	numero = int(input("Introduce un numero positivo mayor a 2: "))
+	
+	esprimo(numero)
