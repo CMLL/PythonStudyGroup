87a7b6781f1364ce9faad10cae9cbde0e9bc64ff
Carlos Llamacho <llamacholancho@gmail.com>
2013-05-29 19:27:38 +0000

    Añadido el archivo para trabajar el desafio a 6 manos de la semana 5.

---
+++ Semana5/Sem5Des1+CarlosLlamacho.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 29 19:14:01 2013
+
+@author: Carlos Llamacho
+
+Desafio Grupal Grupo de Estudio Python G+
+"""
+
+def main():
+	"""Recoge los patrones de diseño y la cantidad de capas."""
+	
+	capas = int(input("Cuantas capas tendra el diseño: "))
+	ancho = int(input("Cual es el ancho del patron: "))
+	
+def capas(capas):
+
+	disenios = []	
+	
+	for i in range(capas):
+		disenios.append(str(input))
+
+if __name__=="__main__":
+	main()
+
